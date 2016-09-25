e4789912ad36c743a8e46805b87196e82c056597
Victor Moreno <thecodingteacher@gmail.com>
2016-09-25 13:21:14 -0400

    feat: working on settings page (Start 15mins ago)

--- static/js/controllers/SettingsController.js
+++ static/js/controllers/SettingsController.js
@@ -1,5 +1,14 @@
 module.exports = function($scope, userService){
   console.log('ReportsController reporting in');
-
+  $scope.user = userService.getUserObject();
   $scope.logOut = ()=>userService.logOut();
+  $scope.newUserData = {id: $scope.user.id,
+                      username: $scope.user.username,
+                      calorie_budget: $scope.user.calorie_budget};
+  $scope.updateUser = function(){
+    if ($scope.newUserData.password){
+      //make sure passwords match
+      //add password to newUserData
+    }
+  }
 };
--- static/js/prod.js
+++ static/js/prod.js
@@ -215,8 +215,17 @@ module.exports = function($scope, userService){
 },{}],7:[function(require,module,exports){
 module.exports = function($scope, userService){
   console.log('ReportsController reporting in');
-
+  $scope.user = userService.getUserObject();
   $scope.logOut = ()=>userService.logOut();
+  $scope.newUserData = {id: $scope.user.id,
+                      username: $scope.user.username,
+                      calorie_budget: $scope.user.calorie_budget};
+  $scope.updateUser = function(){
+    if ($scope.newUserData.password){
+      //make sure passwords match
+      //add password to newUserData
+    }
+  }
 };
 
 },{}],8:[function(require,module,exports){
